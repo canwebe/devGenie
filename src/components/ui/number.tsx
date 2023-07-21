d569f7917d98a067c7a9d243a2fc019d8737cbd9
Raju Khattri <35406115+Kiyoponn@users.noreply.github.com>
2023-07-21 19:29:41 +0530

    👌 IMPROVE: Implemented UI using shadcn/ui

---
+++ src/components/ui/number.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+
+type DivProps = React.HTMLAttributes<HTMLDivElement>
+
+const NumberList = ({ className, children, ...props }: DivProps) => (
+	<div
+		className={cn(
+			'rounded-full bg-primary/80 w-5 h-5 flex justify-center items-center',
+			className
+		)}
+		{...props}
+	>
+		<p className='text-background text-xs'>{children}</p>
+	</div>
+)
+
+export { NumberList }
