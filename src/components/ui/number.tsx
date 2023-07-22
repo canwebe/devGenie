375c1db9af38c6988b00b38b5f1768dabfefa0db
Raju Khattri <35406115+Kiyoponn@users.noreply.github.com>
2023-07-23 00:24:30 +0530

    📦 NEW: Added login modal and toast

--- src/components/ui/number.tsx
+++ src/components/ui/number.tsx
@@ -5,15 +5,15 @@ import { cn } from '@/lib/utils'
 type DivProps = React.HTMLAttributes<HTMLDivElement>
 
 const NumberList = ({ className, children, ...props }: DivProps) => (
-  <div
-    className={cn(
-      'rounded-full bg-primary/80 w-5 h-5 flex justify-center items-center',
-      className
-    )}
-    {...props}
-  >
-    <p className="text-background text-xs">{children}</p>
-  </div>
+	<div
+		className={cn(
+			'rounded-full bg-primary/80 w-5 h-5 flex justify-center items-center',
+			className
+		)}
+		{...props}
+	>
+		<p className='text-background text-xs'>{children}</p>
+	</div>
 )
 
 export { NumberList }
