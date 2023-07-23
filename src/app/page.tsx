3a02b5f0f1bca3fc69edd843fc8ab055985a43d9
devRabbani <dev.golamrabbani@gmail.com>
2023-07-24 02:58:08 +0530

    🐛 FIX: fixed server side fetching issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,23 @@ import MainForm from '@/components/mainForm'
 import Nav from '@/components/nav'
 import Insights from '@/components/insights'
 import ToastClient from '@/components/toastClient'
+import { getGenerationCount, getUsersLists } from '@/utils/firebase-helper'
 
-export default function Home() {
+export const revalidate = 60
+
+export default async function Home() {
+  const countData = getGenerationCount()
+  const usersListData = getUsersLists()
+
+  const [count, usersList] = await Promise.all([countData, usersListData])
+
   console.count('Re render')
 
   return (
     <div className="min-h-screen flex flex-col">
       <Nav />
       <Header />
-      <Insights />
+      <Insights count={count} usersList={usersList} />
       <MainForm />
       <ToastClient />
     </div>
