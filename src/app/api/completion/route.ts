c345892295c36b7389d32c6ddaf50854b49f2bf1
devRabbani <dev.golamrabbani@gmail.com>
2023-07-26 17:01:41 +0530

    📦 NEW: added placeholders and scroll delay

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -9,57 +9,55 @@ import { NextResponse } from 'next/server'
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
-	// Getting the request data
-	const data: Data = await req.json()
+  // Getting the request data
+  const data: Data = await req.json()
 
-	// Destructuring the data object
-	const { prompt, mode, tone, creativity, characters, uid } = data
+  // Destructuring the data object
+  const { prompt, mode, tone, creativity, characters, uid } = data
 
-	// Rate Limiter init
-	const ratelimit = new Ratelimit({
-		redis: Redis.fromEnv(),
-		limiter: Ratelimit.slidingWindow(1, '5 m')
-	})
+  // Rate Limiter init
+  const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(7, '2 m'),
+  })
 
-	const identifier = uid // Limited by userid
+  const identifier = uid // Limited by userid
 
-	const { success, limit, remaining } = await ratelimit.limit(identifier)
+  const { success } = await ratelimit.limit(identifier)
 
-	console.log(`Limit: ${limit}, Remaining: ${remaining}`)
+  if (!success) {
+    return NextResponse.json(
+      { error: 'You are being Rate limited' },
+      { status: 429 }
+    )
+  }
 
-	if (!success) {
-		return NextResponse.json(
-			{ error: 'You are being Rate limited' },
-			{ status: 429 }
-		)
-	}
+  // Getting the prompt based on type
+  const promptText = getPromt(mode, tone, characters, prompt)
 
-	// Getting the prompt based on type
-	const promptText = getPromt(mode, tone, characters, prompt)
+  // Body for POST request
+  const body = JSON.stringify({
+    model: 'command-xlarge-nightly',
+    prompt: promptText,
+    return_likelihoods: 'NONE',
+    max_tokens: 250,
+    temperature: Number(creativity),
+    stream: true,
+  })
 
-	// Body for POST request
-	const body = JSON.stringify({
-		model: 'command-xlarge-nightly',
-		prompt: promptText,
-		return_likelihoods: 'NONE',
-		max_tokens: 250,
-		temperature: Number(creativity),
-		stream: true
-	})
+  // Fetching POST request to cohere api
+  const response = await fetch('https://api.cohere.ai/generate', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.COHERE_API_KEY}`,
+      'Cohere-Version': '2022-12-06',
+    },
+    body,
+  })
 
-	// Fetching POST request to cohere api
-	const response = await fetch('https://api.cohere.ai/generate', {
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${process.env.COHERE_API_KEY}`,
-			'Cohere-Version': '2022-12-06'
-		},
-		body
-	})
+  const stream = CohereStream(response)
 
-	const stream = CohereStream(response)
-
-	return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream)
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { ThemeToggle } from './themeToggle'
+import { useEffect } from 'react'
 
+import { ThemeToggle } from './themeToggle'
 import AuthButton from '@/components/authButton'
 import { Separator } from '@/components/ui/seprator'
 
@@ -9,23 +10,35 @@ import { fontSansCD } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
 
 export default function Nav() {
-	return (
-		<>
-			<nav className='max-w-2xl mx-auto w-full px-4 flex h-16 justify-between items-center'>
-				<h1
-					className={cn(
-						'text-xl font-semibold font-sans-cd',
-						fontSansCD.variable
-					)}
-				>
-					DevGenie
-				</h1>
-				<div className='flex gap-4 items-center'>
-					<ThemeToggle />
-					<AuthButton />
-				</div>
-			</nav>
-			<Separator />
-		</>
-	)
+  useEffect(() => {
+    console.log(
+      '%cCan%cWeBe!',
+      'color: #e47e24; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;',
+      'color: #fff; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;'
+    )
+    console.log(
+      '%cHey explorer!, Are you lost?? Because this is not the right place for you. If you want to work with us at CanWeBe contact us now.',
+      'color: #e1e1e1; font-size: 1.5em;'
+    )
+  }, [])
+
+  return (
+    <>
+      <nav className="max-w-2xl mx-auto w-full px-4 flex h-16 justify-between items-center">
+        <h1
+          className={cn(
+            'text-xl font-semibold font-sans-cd',
+            fontSansCD.variable
+          )}
+        >
+          DevGenie
+        </h1>
+        <div className="flex gap-4 items-center">
+          <ThemeToggle />
+          <AuthButton />
+        </div>
+      </nav>
+      <Separator />
+    </>
+  )
 }
